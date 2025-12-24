c4c6fef3b867deb023a53cbaefcc87cbb80971fa
Anhpro1412 <redminote113963@gmail.com>
2025-12-24 15:16:49 +0700

    add middle ware

--- middlewares/admin/uploadCloud.middleware.js
+++ middlewares/admin/uploadCloud.middleware.js
@@ -1,7 +1,34 @@
+//middlewares/admin/uploadCloud.middleware.js
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 cloudinary.config({
-  cloud_name: "dy0ejkx4g",
-  api_key: "267579526251791",
-  api_secret: "hxFZQF6j65OlE3rfsU3Aw6eqnoQ",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUND_SECRET,
 });
+ module.exports.upload = async  (req, res, next) => {
+    if (req.file) {
+      try {
+        let streamUpload = (req) => {
+          return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream((error, result) => {
+              if (result) resolve(result);
+              else reject(error);
+            });
+
+            streamifier.createReadStream(req.file.buffer).pipe(stream);
+          });
+        };
+
+        let result = await streamUpload(req);
+        // ✅ dùng https
+        req.body[req.file.fieldname] = result.secure_url;
+
+        next();
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      next();
+    }
+  }
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const streamifier = require("streamifier");
 const router = express.Router();
-
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
 cloudinary.config({
   cloud_name: "dy0ejkx4g",
   api_key: "267579526251791",
@@ -26,32 +26,7 @@ router.get("/create", controller.create);
 router.post(
   "/create",
   upload.single("thumbnail"),
-  async function (req, res, next) {
-    if (req.file) {
-      try {
-        let streamUpload = (req) => {
-          return new Promise((resolve, reject) => {
-            let stream = cloudinary.uploader.upload_stream((error, result) => {
-              if (result) resolve(result);
-              else reject(error);
-            });
-
-            streamifier.createReadStream(req.file.buffer).pipe(stream);
-          });
-        };
-
-        let result = await streamUpload(req);
-        // ✅ dùng https
-        req.body[req.file.fieldname] = result.secure_url;
-
-        next();
-      } catch (err) {
-        next(err);
-      }
-    } else {
-      next();
-    }
-  },
+  uploadCloud.upload,
   validate.createPost,
   controller.createPost
 );
